6941c7c69086831c49b376e409085e606bd47716
mcstar <liquid.phynix@gmail.com>
2012-10-07 18:08:42 +0200

    fixed save session

--- okular_session_manager.py
+++ okular_session_manager.py
@@ -5,7 +5,7 @@ if os.getuid() == 0:
     print 'you are root, lets quit'
     exit(-1)
     
-import dbus, dbus.service, dbus.proxies, gobject, json, subprocess
+import dbus, dbus.service, dbus.proxies, gobject, json, subprocess, sys
 from dbus.mainloop.glib import DBusGMainLoop
 
 session_file = os.getenv('HOME') + '/.okularsession'
@@ -23,27 +23,41 @@ def loadSession():
 
 def saveSession():
     with open(session_file, 'w') as f:
-        json.dump(store.values(), f)
+        json.dump(store.values(), f, indent = 1)
 
+def getDoc(name):
+    proxy = dbus.proxies.ProxyObject(conn = bus.get_connection(), bus_name = name, object_path = '/okular')
+    return proxy.currentDocument(dbus_interface = 'org.kde.okular')
+        
 def nameOwnerChanged(name, old_owner, new_owner):
     if not name.startswith('org.kde.okular'): return
     if new_owner == "":
         del store[name]
     else:
-        proxy = dbus.proxies.ProxyObject(conn = bus.get_connection(), bus_name = name, object_path = '/okular')
-        doc = proxy.currentDocument(dbus_interface = 'org.kde.okular')
+        doc = getDoc(name)
         if doc != "":
             store[name] = doc
-            saveSession()
-        
+    saveSession()
+
 if __name__ == '__main__':
     DBusGMainLoop(set_as_default = True)
     try:
         bus = dbus.SessionBus()
+        if len(sys.argv) == 2 and sys.argv[1] == 'update':
+            proxy = dbus.proxies.ProxyObject(conn = bus.get_connection(), bus_name = 'org.freedesktop.DBus', object_path = '/org/freedesktop/DBus')
+            for name in proxy.ListNames(dbus_interface = 'org.freedesktop.DBus'):
+                if name.startswith('org.kde.okular'):
+                    doc = getDoc(name)
+                    if doc != "":
+                        store[name] = doc
+            saveSession()
+            exit(0)
+            
     except dbus.DBusException:
         print 'cannot connect to sessionbus'
     try:
         myBus = dbus.service.BusName('org.mcstar.okularsession', bus = bus, do_not_queue = True)
+
         loop = gobject.MainLoop()
         bus.add_signal_receiver(nameOwnerChanged, dbus_interface = "org.freedesktop.DBus", signal_name = "NameOwnerChanged")
         try:
